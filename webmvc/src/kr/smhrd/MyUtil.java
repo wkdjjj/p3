1fe0e19928aba463c7c1b4ae27cff18190efc878
wkdjj <wkdjj@DESKTOP-LCOSDUG>
2020-09-07 16:44:43 +0900

    수정완료

--- webmvc/src/kr/smhrd/MyUtil.java
+++ webmvc/src/kr/smhrd/MyUtil.java
@@ -4,8 +4,8 @@ public class MyUtil {
 	// 1~10까지의 총합을 구하는 동작
 	public int hap() {
 		int sum =0;
-		for(int i=1; i<=10;i++) {
-			// 용채야 여기좀 수정해봐
+		for(int i=1; i<=1000;i++) {
+			sum+=i;
 		}
 		return sum;
 	}
